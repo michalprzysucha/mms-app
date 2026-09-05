ebc7f0ac54aa4b745dfada17e731404175683cda
michalprzysucha <mpbeliar@gmail.com>
2026-09-05 19:05:19 +0200

    [OMA-10] Add Spring Boot minimal dependencies and simple controller for test purposes.

---
+++ src/main/java/com/onemanarmy/mms/MmsApplication.java
@@ -0,0 +1,11 @@
+package com.onemanarmy.mms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class MmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MmsApplication.class, args);
+    }
+}
