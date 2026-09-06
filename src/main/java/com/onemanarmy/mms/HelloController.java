c9d461bcacda2d3fdd69bedfbb1868deb7be26ff
michalprzysucha <mpbeliar@gmail.com>
2026-09-06 16:02:43 +0200

    Upgrade Java to 25 and gradlew to 9.7.1.

--- src/main/java/com/onemanarmy/mms/HelloController.java
+++ src/main/java/com/onemanarmy/mms/HelloController.java
@@ -1,11 +1,13 @@
 package com.onemanarmy.mms;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
 class HelloController {
+
+    @GetMapping("/hello")
     String helloEndpoint() {
         return "Hello World!";
     }
