d05e1a786a75aafcd4997322065bd6b47f84b6e9
micprz99 <michal.przysucha@circlekeurope.com>
2026-09-15 10:53:59 +0200

    test commit

--- src/main/java/com/onemanarmy/mms/MmsApplication.java
+++ src/main/java/com/onemanarmy/mms/MmsApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MmsApplication {
+
+    //test
     public static void main(String[] args) {
         SpringApplication.run(MmsApplication.class, args);
     }
